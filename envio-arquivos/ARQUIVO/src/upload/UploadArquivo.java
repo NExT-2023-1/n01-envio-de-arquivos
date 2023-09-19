4de5a80dc95db4bead9f05ce5f684829b80587af
Júlio Marques <juliomarques170@gmail.com>
2023-09-18 21:21:02 -0300

    Criado o gitignore

--- envio-arquivos/ARQUIVO/src/upload/UploadArquivo.java
+++ envio-arquivos/ARQUIVO/src/upload/UploadArquivo.java
@@ -3,7 +3,7 @@ package upload;
 import java.io.*;
 import java.net.*;
 
-public class UploadaArquivo {
+public class UploadArquivo {
 
     public static void main(String[] args) throws Exception {
         String url = "http://example.com/upload";
@@ -18,15 +18,6 @@ public class UploadaArquivo {
         int bytesRead;
         while ((bytesRead = inputStream.read(buffer)) > 0) {
             partsUploaded++;
-           //URLConnection connection = new URL(url).openConnection();
-            /*connection.setRequestMethod("POST");
-            //connection.setRequestProperty("Content-Type", "application/octet-stream");
-            //connection.setRequestProperty("Content-Length", String.valueOf(bytesRead));
-            //connection.setRequestProperty("Content-Range", "bytes " + ((partsUploaded - 1) * partSize) + "-" + ((partsUploaded - 1) * partSize + bytesRead - 1) + "/" + fileSize);
-            //connection.setDoOutput(true);
-            OutputStream outputStream = connection.getOutputStream();
-            outputStream.write(buffer, 0, bytesRead);
-            outputStream.close();*/
             System.out.println("Tamanho parte upload " + partsUploaded + " : " + buffer.length );
             System.out.println("Parte " + partsUploaded + " de " + totalParts + " uploaded.");
         }
